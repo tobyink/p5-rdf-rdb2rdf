dbe553957ba09d3cd19bfbfbe7f522df9768d8d7
Toby Inkster <mail@tobyinkster.co.uk>
2013-09-20 11:58:15 +0100

    don't need to leave this lying around
    --HG--
    branch : RDF-RDB2RDF

--- t/update-books.sql
+++
@@ -1,5 +0,0 @@
-alter table books add column title_lang TEXT;
-update books set title="Cooking provincial français" where book_id=1;
-update books set title_lang="fr" where book_id=1;
-update books set title="Cibo Italiano" where book_id=2;
-update books set title_lang="it" where book_id=2;
